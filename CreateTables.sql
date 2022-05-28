8752070440214e48efcb105b7d9fbe776ef7be12
Pedro Cortez <pedrocortez877@gmail.com>
2022-05-28 00:30:31 -0300

    feat: create database and tables

---
+++ CreateTables.sql
@@ -0,0 +1,51 @@
+CREATE TABLE public."Users"
+(
+  "Email" text NOT NULL,
+  "Password" text NOT NULL,
+  "Name" text NOT NULL
+);
+
+CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE public."Restaurants"
+(
+    "Id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+    "Name" text NOT NULL,
+    "FoodType" text NOT NULL,
+    "Evaluation" numeric NOT NULL,
+    "DeliveryTime" text NOT NULL,
+    "DeliveryValue" numeric NOT NULL,
+    "ProfileImage" text NOT NULL,
+    "BackgroundImage" text NOT NULL,
+    PRIMARY KEY ("Id")
+);
+
+CREATE TABLE public."ProductCategories"
+(
+    "Id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+    "Name" text NOT NULL,
+    PRIMARY KEY ("Id")
+);
+
+CREATE TABLE public."Products"
+(
+    "Id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+    "Name" text NOT NULL,
+    "Description" text,
+    "Price" numeric NOT NULL,
+    "IdCategory" uuid NOT NULL,
+    "IdRestaurant" uuid NOT NULL,
+    "Image" text NOT NULL,
+    PRIMARY KEY ("Id"),
+    CONSTRAINT fk_product_category FOREIGN KEY ("IdCategory")
+        REFERENCES public."ProductCategories" ("Id") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT fk_product_restaurant FOREIGN KEY ("IdRestaurant")
+        REFERENCES public."Restaurants" ("Id") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
