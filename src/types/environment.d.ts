1fed44d496a9b6f2f24f0470a5d2bad8826f58eb
Pedro Cortez <pedrocortez877@gmail.com>
2022-05-29 09:26:48 -0300

    feat: typing environment variables

---
+++ src/types/environment.d.ts
@@ -0,0 +1,16 @@
+export {};
+
+declare global {
+  namespace NodeJS{
+    interface ProcessEnv{
+      PORT: number;
+      BASE_HOST: string;
+      BASE_PATH: string;
+      DB_USER: string;
+      DB_PASSWORD: string;
+      DB_NAME: string;
+      DB_HOST: string;
+      DB_DIALECT: string;
+    }
+  }
+}
